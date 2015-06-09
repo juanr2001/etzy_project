33c2096d9dbc1c3a474f08b544a159f5bc25f951
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-06-09 07:57:30 -0400

    Done sidebar and footer, header

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -1,9 +1,25 @@
-require "etsiest/version"
 require 'pry'
 require 'json'
 require 'sinatra/base'
+require 'etsy'
+require "etsiest/version"
+
+Etsy.api_key = ENV['ETSY_KEY']
+#4ur39qgfjkrpiogytd6va45i
 
 module Etsiest
-  # Your code goes here...
+    class App < Sinatra::Base
+
+
+        get '/search' do
+            response = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => 'cheese')
+            @results = response.result
+            erb :index
+        end
+
+        run! if app_file == $0
+
+    end
+binding.pry
 end
 
